773130525418256aed736103a57d54f325a93216
josone9 <codec-fowls.0r@icloud.com>
2023-12-31 03:03:04 +0000

    Initial commit on main branch

---
+++ Driving_Age.py
@@ -0,0 +1,32 @@
+class Adult:
+    def __init__(self, name, age, eye_colour, hair_colour):
+        self.name = name
+        self.age = age
+        self.eye_colour = eye_colour
+        self.hair_colour = hair_colour
+
+    def can_drive(self):
+        print(f"{self.name} is old enough to drive.")
+
+class Child(Adult):
+    def can_drive(self):
+        print(f"{self.name} is too young to drive.")
+
+def main():
+    # Take user inputs
+    name = input("Enter the name: ")
+    age = int(input("Enter the age: "))
+    eye_colour = input("Enter the eye colour: ")
+    hair_colour = input("Enter the hair colour: ")
+
+    # Determine if the person is 18 or older
+    if age >= 18:
+        person = Adult(name, age, eye_colour, hair_colour)
+    else:
+        person = Child(name, age, eye_colour, hair_colour)
+
+    # Call the can_drive() method
+    person.can_drive()
+
+if __name__ == "__main__":
+    main()
